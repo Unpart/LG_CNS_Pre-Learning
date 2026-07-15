afc676806c39b42c2e1dc27cbf97b8aefe7adc10
unpart <mhy7847@naver.com>
2026-07-15 19:09:30 +0900

    260715 사전학습 10일차 커밋

---
+++ 260715/VariableApp.java
@@ -0,0 +1,9 @@
+public class VariableApp {
+    
+    // 전역변수
+    public String name = "jslim";
+    public int    age = 24;
+    public double height = 178.6;
+    public boolean isMarraige = true;
+    public char gender = 'm';
+}
